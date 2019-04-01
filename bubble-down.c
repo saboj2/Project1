ac1e0e7d598a6b93edf55f0ec9e177d9cd255493
Jeffrey Sabo <saboj2@tcnj.edu>
2019-03-31 20:06:09 -0400

    both bubble-up and bubble-down working

--- bubble-down.c
+++ bubble-down.c
@@ -1,4 +1,50 @@
+#include <stdio.h>
+
+void bubble_down(long* heap, long k, long n);
+
 int main(int argc, char** argv)
 {
+//Read in from std in
+    //get size n from first line
+    long n;
+    scanf("%ld", &n); 
+    long heap[n];
 
+    //fill up array
+    long i; 
+    for(i = 0; i < n; ++i) {
+       scanf("%ld", &heap[i]);
+    }
+    
+//Bubble Down logic
+    for(i = n/2; i >= 0; --i) {
+        bubble_down(heap, i, n);
+    }
+//Output to std out
+    for(i = 0; i < n; ++i) {
+        printf("%ld\n", heap[i]);
+    }
+    
+    return 0;
+}
+
+void bubble_down(long* heap, long k, long n)
+{
+    long temp;
+
+    if(heap[k] > heap[2*k+1] && 2*k+1 < n) {
+        temp = heap[k];
+        heap[k] = heap[2*k+1];
+        heap[2*k+1] = temp;  
+        bubble_down(heap, 2*k+1, n); 
+    }
+
+    if(heap[k] > heap[2*k+2] && 2*k+2 < n) {
+        temp = heap[k];
+        heap[k] = heap[2*k+2];
+        heap[2*k+2] = temp;  
+        bubble_down(heap, 2*k+2, n); 
+    }
+        
+    return; 
 }
--- bubble-up.c
+++ bubble-up.c
@@ -1,4 +1,49 @@
+#include <stdio.h>
+
+void bubble_up(long* heap, long k);
+
 int main(int argc, char** argv)
 {
+//Read in from std in
+    //get size n from first line
+    long n;
+    scanf("%ld", &n); 
+    long heap[n];
 
+    //fill up array
+    long i; 
+    for(i = 0; i < n; ++i) {
+       scanf("%ld", &heap[i]);
+    }
+
+//Bubble UpHeap Construction 
+//Start at zero? First element is already a heap, doesnt need to be manipulated by itself
+    for(i = 1; i < n; i++) {
+       if(heap[i] < heap[(i-1)/2] && i != 0)
+            bubble_up(heap, i); 
+    }
+//Output to std out
+    for(i = 0; i < n; ++i) {
+        printf("%ld\n", heap[i]);
+    }
+    
+    return 0;
+}
+
+
+void bubble_up(long* heap, long k)
+{
+    //swap two values that need to swapped
+    long temp = heap[k];
+    heap[k] = heap[(k-1)/2];
+    heap[(k-1)/2] = temp;
+    
+    
+    //check to see if its parent is still bigger, call swap recursively if it is
+    
+    //start at 1 to make this simpler?
+    if(heap[(k-1)/2] < heap[(k-3)/4])
+        bubble_up(heap, (k-1)/2);
+        
+    return;
 }
