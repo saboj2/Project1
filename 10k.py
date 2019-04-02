07ab50fda90781f5076327939fffac30aae8c9b9
sancher6 <sancher6@tcnj.edu>
2019-04-02 10:56:44 -0400

    updated python script

--- 10k.py
+++ 10k.py
@@ -3,7 +3,10 @@ import random
 file = open("Random.txt", "w")
 
 for x in range(10000):
-    num = str(random.randint(1,100))
-    file.write(num + "\n")
+    if x == 0: 
+        file.write("10000\n")
+    else: 
+        num = str(random.randint(1,100))
+        file.write(num + "\n")
 
 file.close()
