9ea671956da934a5e8cbf2408f36ba26da7b6653
sancher6 <sancher6@tcnj.edu>
2019-04-02 13:15:18 -0400

    updates to text files

--- 10k.py
+++ 10k.py
@@ -1,12 +1,15 @@
 import random
 
-file = open("Random.txt2", "w")
+file = open("Random2.txt", "w")
 
-for x in range(10000):
+for x in range(10001):
     if x == 0: 
         file.write("10000\n")
-    else: 
-        num = str(10000-x)
+    elif x < 5000:
+        num = str(random.randint(0,4999))
         file.write(num + "\n")
+    else:
+        num = str(random.randint(5000,10001))
+        file.write(num + "\n") 
 
 file.close()
